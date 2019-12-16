81657b7ed2e2b6c38c915fb2d544915387c854fd
liyuwangluo <liyu2016520@163.com>
2019-12-16 13:22:53 +0800

    1

---
+++ configclient/src/main/java/com/liyu/ConfigHandler.java
@@ -0,0 +1,19 @@
+package com.liyu;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class ConfigHandler {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/index")
+    public String index(){
+        return this.port;
+    }
+}
